a252e54b80532b59ad882d4e887d478804efb289
Edward Flores <scherenhaenden@gmail.com>
2021-01-01 22:18:08 +0100

    Add Avalonia UI

---
+++ Blazares.Gallery.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazares.Gallery.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
